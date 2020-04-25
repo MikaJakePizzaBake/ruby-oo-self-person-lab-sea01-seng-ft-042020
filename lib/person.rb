b8e7687ce0d408ba9caafe2fce10a8dd28dc859f
Trevor Low <tlow94@uw.edu>
2020-04-25 23:48:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -46,6 +46,6 @@ def call_friend(friend)
   "Hi #{friend.name}! It's #{name}. How are you?"
 end
 def start_conversation (person, topic)
-  
+  'blah blah partisan blah lobbyist' if topic = "politics"
 end
 end
